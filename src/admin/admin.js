509b09d345789bc1db7fa684d8127dfac8c8354f
amiramohiey <ameramohiey92@gmail.com>
2017-05-17 10:04:12 +0200

    adding rules crud to admin

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -12,6 +12,7 @@ import UserIcon from 'material-ui/svg-icons/social/group';
 import TrackIcon from 'material-ui/svg-icons/communication/import-contacts';
 import './admin.css';
 import authClient from '../authClient'
+import RuleIcon from 'material-ui/svg-icons/action/event'
 import { Delete } from 'admin-on-rest';
 class AttendenceAdmin extends Component 
 
@@ -32,7 +33,7 @@ class AttendenceAdmin extends Component
         return (
       
     <Admin authClient={authClient} title="Attendance Admin" restClient={ myApiRestClient} >
-        <Resource name="rules" list={RuleList} icon={PostIcon} edit={RuleEdit} create={RuleCreate} remove={Delete}/>    
+        <Resource name="rules" list={RuleList} icon={RuleIcon} edit={RuleEdit} create={RuleCreate} remove={Delete}/>    
 
         <Resource name="branches" list={BranchList} icon={PostIcon} edit={BranchEdit} create={BranchCreate} remove={Delete}/>    
         <Resource name="tracks" list={TrackList} icon={TrackIcon} edit={TrackEdit} create={TrackCreate} remove={Delete}/>
